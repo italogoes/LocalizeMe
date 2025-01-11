b944e530db03ba2da77bf6a78587dfc9d3afb48d
Italo Goes <italo.goes@geosiga.com.br>
2025-01-11 02:10:46 -0300

    new build

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -7,14 +7,18 @@ exports.app = void 0;
 const express_1 = __importDefault(require("express"));
 exports.app = (0, express_1.default)();
 require("./server");
-exports.app.get('/', (req, res) => {
-    res.send('Ola');
+let data = [];
+exports.app.get('/location', (req, res) => {
+    res.send(data);
 });
 exports.app.post('/location', (req, res) => {
-    const { lat, lon } = req.body;
-    if (!lat || !lon) {
+    const { latitude, longitude } = req.body;
+    if (!latitude || !longitude) {
         return res.status(400).json({ error: "Localização inválida!" });
     }
     res.status(200).json({ message: "Localização recebida com sucesso!" });
-    console.log(`${lon} e ${lat}`);
+    data = [];
+    data.push(latitude);
+    data.push(longitude);
+    console.log(data);
 });
